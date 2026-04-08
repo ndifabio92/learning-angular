b47692618614e9ac86e325b0006875dd06fc7a15
Nicolas Di Fabio <ndifabio@icebergsolutions.com>
2026-04-08 17:13:27 -0300

    184

--- 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.ts
@@ -1,9 +1,49 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.html',
 })
-export class BasicPage {}
+export class BasicPage {
+  private fb = inject(FormBuilder);
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: ['', [Validators.required, Validators.min(10)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
+  });
+  // myForm = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   isStorage: new FormControl(0),
+  // });
+
+  isValidField(fieldName: string): boolean | null {
+    return !!this.myForm.controls[fieldName].errors;
+  }
+
+  getFieldError(fieldName: string): string | null {
+    if (!this.myForm.controls[fieldName]) return null;
+    const errors = this.myForm.controls[fieldName].errors ?? {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Minimo de ${errors['minlength'].requiredLength} caracteres`;
+        case 'min':
+          return `Valor minimo de ${errors['min'].min}`;
+      }
+    }
+    return null;
+  }
+}
