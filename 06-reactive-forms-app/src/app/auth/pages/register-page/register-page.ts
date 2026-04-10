a1e6c29f31d28402506fa2cf30bd3d243400ab84
Nicolas Di Fabio <ndifabio@icebergsolutions.com>
2026-04-10 14:21:49 -0300

    end section 15

--- 06-reactive-forms-app/src/app/auth/pages/register-page/register-page.ts
+++ 06-reactive-forms-app/src/app/auth/pages/register-page/register-page.ts
@@ -1,9 +1,44 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-register-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './register-page.html',
 })
-export class RegisterPage {}
+export class RegisterPage {
+  private fb = inject(FormBuilder);
+
+  formUtils = FormUtils;
+
+  myForm = this.fb.group(
+    {
+      name: ['', [Validators.required, Validators.pattern(this.formUtils.namePattern)]],
+      email: [
+        '',
+        [Validators.required, Validators.pattern(FormUtils.emailPattern)],
+        [FormUtils.checkingServerResponse],
+      ],
+      username: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.pattern(this.formUtils.notOnlySpacesPattern),
+        ],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
+    },
+    {
+      validators: [this.formUtils.isFieldOneEqualFieldTwo('password', 'confirmPassword')],
+    },
+  );
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+    console.log(this.myForm.value);
+  }
+}
--- 06-reactive-forms-app/src/app/reactive/pages/switches-pages/switches-pages.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/switches-pages/switches-pages.ts
@@ -1,9 +1,26 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-switches-pages',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-pages.html',
 })
-export class SwitchesPages {}
+export class SwitchesPages {
+  private fb = inject(FormBuilder);
+
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+    console.log(this.myForm.value);
+  }
+}
